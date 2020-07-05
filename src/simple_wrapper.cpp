e2da73bc600db4c088d4bce412867a11bced2216
Daniel Lemire <lemire@gmail.com>
2020-07-05 16:37:01 -0400

    Tweaking

--- src/simple_wrapper.cpp
+++ src/simple_wrapper.cpp
@@ -1,11 +1,41 @@
 #include "simdjson.h"
+#include <chrono>
 
-bool is_ok(const char * filename) {
+
+// simdjson is designed so that you reuse the same parser.
+simdjson::dom::parser parser;
+
+bool is_ok(const char *filename) {
+  std::chrono::steady_clock::time_point begin, end;
+  std::chrono::duration<double> elapsed;
+  
+  simdjson::padded_string docdata;
+  begin = std::chrono::steady_clock::now();
+  auto error = simdjson::padded_string::load(filename).get(docdata);
+  if (error) {
+    std::cerr << "could not parse " << filename << ":" << error << std::endl;
+    return false;
+  }
+  end = std::chrono::steady_clock::now();
+  elapsed = end - begin;
+  double gb = double(docdata.size()) / 1000000000.;
+
+  std::cout << "loading at " << gb / elapsed.count() << " GB/s" << std::endl;
+
+  for (size_t i = 0; i < 3; i++) {
+    std::cout << "trial one : " << i << std::endl;
+    begin = std::chrono::steady_clock::now();
     simdjson::dom::element doc;
-    simdjson::dom::parser parser;
-    auto error = parser.load(filename).get(doc);
-    if (error) { std::cerr << error << std::endl; return false;}
-    return true;
+    if ((error = parser.parse(docdata).get(doc))) {
+      std::cerr << "could not parse " << filename << ":" << error << std::endl;
+      return false;
+    }
+    end = std::chrono::steady_clock::now();
+    elapsed = end - begin;
+
+    std::cout << "parsing at " << gb / elapsed.count() << " GB/s" << std::endl;
+  }
+  return true;
 }
 
 extern "C" {
@@ -13,50 +43,33 @@ extern "C" {
 #include <Python.h>
 
 static PyObject *check_if_valid(PyObject *self, PyObject *args) {
-    char *char_values;
-    if (!PyArg_ParseTuple(args, "s", &char_values)) {
-        Py_RETURN_NONE;
-    }
-    if(is_ok(char_values)) {
-      Py_RETURN_TRUE;
-    } else {
-      Py_RETURN_FALSE;  
-    }
+  char *char_values;
+  if (!PyArg_ParseTuple(args, "s", &char_values)) {
+    Py_RETURN_NONE;
+  }
+  if (is_ok(char_values)) {
+    Py_RETURN_TRUE;
+  } else {
+    Py_RETURN_FALSE;
+  }
 }
 
 static PyMethodDef simdjson_methods[] = {
-     {"is_valid", check_if_valid, METH_VARARGS, "is valid"},
-     {NULL, NULL, 0, NULL}
-};
-
-
+    {"is_valid", check_if_valid, METH_VARARGS, "is valid"},
+    {NULL, NULL, 0, NULL}};
 
 #if PY_MAJOR_VERSION >= 3
 
-static struct PyModuleDef cModsimdjson =
-{
-    PyModuleDef_HEAD_INIT,
-    "simdjson",
-    "",
-    -1,
-    simdjson_methods
-};
+static struct PyModuleDef cModsimdjson = {PyModuleDef_HEAD_INIT, "simdjson", "",
+                                          -1, simdjson_methods};
 
-
-PyMODINIT_FUNC
-PyInit_simdjson(void)
-{
-     return PyModule_Create(&cModsimdjson);
-}
+PyMODINIT_FUNC PyInit_simdjson(void) { return PyModule_Create(&cModsimdjson); }
 
 #else
 
-PyMODINIT_FUNC
-initsimdjson(void)
-{
-     (void) Py_InitModule("simdjson", simdjson_methods);
+PyMODINIT_FUNC initsimdjson(void) {
+  (void)Py_InitModule("simdjson", simdjson_methods);
 }
 
 #endif
-
 }
